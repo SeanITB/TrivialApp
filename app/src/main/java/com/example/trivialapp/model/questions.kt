ca21053f0d3f15246db8646095c4d0ae0408b70b
SeanITB <sean.costelloe.7e5@itb.cat>
2024-01-23 16:17:02 +0100

    midium and difficult questions

--- app/src/main/java/com/example/trivialapp/model/questions.kt
+++ app/src/main/java/com/example/trivialapp/model/questions.kt
@@ -30,9 +30,55 @@ enum class easyQuestions(
 
 enum class normalQuestions(
     val img: Int,
+    val question: String,
     val answers: Array<String>,
     val raightAnswear: String
-)
+) {
+    babyGoat(
+        R.drawable.mediu1,
+        "What is a baby goat called?",
+        arrayOf("Cabrito", "Goti", "Small sheep", "Kid"),
+        "Kid"
+    ),
+    hannahMontana(
+        R.drawable.midium2,
+        "What was the name of Miley Cyrus's Disney character?",
+        arrayOf("Jessie", "Hannah Montana", "Bella Thorne", "Cenicienta"),
+        "Hannah Montana"
+    ),
+    brieCheese(
+        R.drawable.mediun3,
+        "What country is Brie cheese originally from",
+        arrayOf("Spain", "Germany", "France", "Luxembourg"),
+        "France"
+    )
+}
+
+enum class difficultQuestions(
+    val img: Int,
+    val question: String,
+    val answers: Array<String>,
+    val raightAnswear: String
+) {
+    nationalFlags(
+        R.drawable.difficult1,
+"What color do about 75 percent of national flags contain?",
+        arrayOf("Blue", "Red", "Green", "Yellow"),
+        "Red"
+    ),
+    weimarRerpublic(
+        R.drawable.difficult2,
+        "From 1919 to 1933, the Weimar Republic was the government of what nation?",
+        arrayOf("Russia", "Germany", "France", "Ukraine"),
+        "Germany"
+    ),
+    erosionRock(
+        R.drawable.difficult3,
+        "What is the wearing away of rock by wind, water or gravity called?",
+        arrayOf("Declension", "Erosion", "Grating", "Silting"),
+        "Erosion"
+    )
+}
 
 
 
